964f097b7715ea53b37bb315c6c6860e6e9a240d
santiago <santiag.2016@gmail.com>
2025-10-27 20:56:34 -0500

    añadi un procedimiento almacenado de compras

--- procedimientos almacenados.sql
+++ procedimientos almacenados.sql
@@ -1,2 +1,20 @@
 --Implementacion de Procedimientos almacenados--
 
+USE AlquimiaLiteraria
+
+--CREA UNA NUEVA COMPRA--
+
+CREATE PROCEDURE InsertarCompra
+@id_cliente INT,
+@id_libro INT,
+@fecha_compra DATE
+
+AS
+BEGIN
+INSERT INTO Compras (id_cliente,id_libro,fecha_compra)
+VALUES(@id_cliente,@id_libro,@fecha_compra);
+END;
+
+EXEC InsertarCompra 1, 3, '2025-10-27';
+
+SELECT * FROM Compras;
